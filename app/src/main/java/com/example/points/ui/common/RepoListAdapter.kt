1de006c5ac225c258559b0ddf5714d027fc85c21
Sergey <zakharovsergey1000@gmail.com>
2021-08-29 10:48:49 +0300

    package com.android.example is renamed to com.example

---
+++ app/src/main/java/com/example/points/ui/common/RepoListAdapter.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.points.ui.common
+
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.points.AppExecutors
+import com.example.points.R
+import com.example.points.databinding.RepoItemBinding
+import com.example.points.vo.Point
+
+/**
+ * A RecyclerView adapter for [Point] class.
+ */
+class RepoListAdapter(
+    private val dataBindingComponent: DataBindingComponent,
+    appExecutors: AppExecutors,
+    private val showFullName: Boolean,
+    private val repoClickCallback: ((Point) -> Unit)?
+) : DataBoundListAdapter<Point, RepoItemBinding>(
+    appExecutors = appExecutors,
+    diffCallback = object : DiffUtil.ItemCallback<Point>() {
+        override fun areItemsTheSame(oldItem: Point, newItem: Point): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Point, newItem: Point): Boolean {
+            return oldItem.x == newItem.x
+                    && oldItem.y == newItem.y
+        }
+    }
+) {
+
+    override fun createBinding(parent: ViewGroup): RepoItemBinding {
+        val binding = DataBindingUtil.inflate<RepoItemBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.repo_item,
+            parent,
+            false,
+            dataBindingComponent
+        )
+        binding.showFullName = showFullName
+        binding.root.setOnClickListener {
+            binding.repo?.let {
+                repoClickCallback?.invoke(it)
+            }
+        }
+        return binding
+    }
+
+    override fun bind(binding: RepoItemBinding, item: Point) {
+        binding.repo = item
+    }
+}
