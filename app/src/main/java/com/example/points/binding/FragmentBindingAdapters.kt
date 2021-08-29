e548993c39326be78366030190cccb820ce34b97
Sergey <zakharovsergey1000@gmail.com>
2021-08-29 11:34:15 +0300

    code cleanup

--- app/src/main/java/com/example/points/binding/FragmentBindingAdapters.kt
+++ app/src/main/java/com/example/points/binding/FragmentBindingAdapters.kt
@@ -17,12 +17,12 @@
 package com.example.points.binding
 
 import android.graphics.drawable.Drawable
-import androidx.fragment.app.Fragment
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.example.points.testing.OpenForTesting
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestListener
+import com.example.points.testing.OpenForTesting
 import javax.inject.Inject
 
 /**
